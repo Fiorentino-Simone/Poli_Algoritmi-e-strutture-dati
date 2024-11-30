27441ea91744a968ad30b2270ea3765133cd9107
Simone Fiorentino <simone.fiorentino@cambieri.it>
2024-11-30 14:54:02 +0100

    Close ese01, lab04

--- Lab04/Ese01/main.c
+++ Lab04/Ese01/main.c
@@ -2,7 +2,8 @@
 #include <stdlib.h>
 
 // TYPEDEF
-typedef struct {
+typedef struct
+{
     int u, v;
 } Arco;
 
@@ -10,16 +11,24 @@ typedef struct {
 void read_file(char *filename, int *N, int *E, Arco **archi);
 
 void powerset(int pos, int i, Arco *val, int *sol, int *vertici, int n, int e);
-int isVertexCover(int *sol, int i, Arco *val, int e);
+int checkVertexCover(int *sol, int i, Arco *val, int e);
 
-int main() {
+int main()
+{
+    // DICHIARAZIONI
     int N, E; // N --> NODI, E --> ARCHI
-    Arco *archi = NULL;
+    int *sol, *vertici;
+    Arco *archi;
+
+    archi = NULL;
 
     read_file("grafo.txt", &N, &E, &archi);
 
-    int sol[N], vertici[N];
-    for (int i = 0; i < N; ++i) {
+    sol = (int *)malloc(N * sizeof(int));
+    vertici = (int *)malloc(N * sizeof(int));
+
+    for (int i = 0; i < N; ++i)
+    {
         vertici[i] = i;
     }
 
@@ -27,72 +36,84 @@ int main() {
     powerset(0, 0, archi, sol, vertici, N, E);
 
     free(archi);
+    free(sol);
+    free(vertici);
     return 0;
 }
 
 /* FUNCTIONS */
-void read_file(char *filename, int *N, int *E, Arco **archi){
+void read_file(char *filename, int *N, int *E, Arco **archi)
+{
     FILE *fin;
 
     fin = fopen(filename, "r");
-    if(fin == NULL){
+    if (fin == NULL)
+    {
         printf("Errore nella lettura del file!");
-        exit(-1);
+        exit(1);
     }
 
     fscanf(fin, "%d %d", N, E);
-    *archi = (Arco *) malloc((*E) * sizeof(Arco));
+    *archi = (Arco *)malloc((*E) * sizeof(Arco));
 
-    for (int i = 0; i < (*E); i++) {
+    for (int i = 0; i < (*E); i++)
+    {
         fscanf(fin, "%d %d", &((*archi)[i].u), &((*archi)[i].v));
     }
 
     fclose(fin);
 }
 
-void powerset(int pos, int i, Arco *val, int *sol, int *vertici, int n, int e){
-    if(pos >= n){
-        if(isVertexCover(sol, i, val, e) == 1){
+void powerset(int pos, int i, Arco *val, int *sol, int *vertici, int n, int e)
+{
+    if (pos >= n) // Condizione di terminazione
+    {
+        if (checkVertexCover(sol, i, val, e) == 1)
+        {
             printf("(");
-            for (int j = 0; j < i; j++) {
-                if((j+1) == i){
-                    printf("%d) ", sol[j]);
-                } else {
-                    printf("%d, ", sol[j]);
-                }
+            for (int j = 0; j < i; j++)
+            {
+                printf("%d ", sol[j]);
             }
+            printf(") ");
         }
         return;
     }
 
     // Prendo il vertice corrente
     sol[i] = vertici[pos];
-    powerset(pos+1, i+1, val, sol, vertici, n, e);
+    powerset(pos + 1, i + 1, val, sol, vertici, n, e);
 
     // Non prendo il vertice corrente
-    powerset(pos+1, i, val, sol, vertici, n, e);
+    powerset(pos + 1, i, val, sol, vertici, n, e);
 }
 
-int isVertexCover(int *sol, int i, Arco *val, int e){
-    int trovato[e];
+int checkVertexCover(int *sol, int i, Arco *val, int e)
+{
+    int *trovato;
 
-    for (int j = 0; j < e; j++) {
-        trovato[j] = 0;
-    }
+    trovato = (int *)calloc(e, sizeof(int));
 
-    for (int j = 0; j < i; j++) {
-        for (int k = 0; k < e; k++) {
-            if(val[k].u == sol[j] || val[k].v == sol[j]){
+    for (int j = 0; j < i; j++)
+    {
+        for (int k = 0; k < e; k++)
+        {
+            if (val[k].u == sol[j] || val[k].v == sol[j])
+            {
                 trovato[k] = 1;
             }
         }
     }
 
-    for (int j = 0; j < e; j++) {
-        if(trovato[j] == 0){
+    for (int j = 0; j < e; j++)
+    {
+        if (trovato[j] == 0)
+        {
+            free(trovato);
             return 0;
         }
     }
 
+    free(trovato);
     return 1;
 }
