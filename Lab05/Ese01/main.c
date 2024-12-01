4c2710b58906b1aa9f4a55724ac59f51bb39dc95
Simone Fiorentino <simone.fiorentino@cambieri.it>
2024-12-01 10:44:18 +0100

    Close ese01, lab05

--- Lab05/Ese01/main.c
+++ Lab05/Ese01/main.c
@@ -2,23 +2,25 @@
 #include <stdlib.h>
 
 // TYPEDEF
-typedef struct{
+typedef struct
+{
     int s, f;
 } Att;
 
 // PROTOTIPI
-void read_data(int *n, Att **v);
+void read_file(int *n, Att **v);
 
 void attSel(int n, Att *v);
 void powerset(int pos, Att *v, int n, int *sol, int *bestSol, int *bestDur, int currDur);
 int check(Att *v, int *sol, int n);
 
-int main() {
-    /* DICHIARAZIONE */
+int main()
+{
+    // DICHIARAZIONE
     int n;
     Att *v;
 
-    read_data(&n, &v);
+    read_file(&n, &v);
 
     attSel(n, v);
 
@@ -27,36 +29,42 @@ int main() {
 }
 
 // FUNCTIONS
-void read_data(int *n, Att **v){
+void read_file(int *n, Att **v)
+{
     FILE *fin;
 
     fin = fopen("att.txt", "r");
-    if(fin == NULL){
+    if (fin == NULL)
+    {
         printf("Impossibile aprire il file!");
-        exit(-1);
+        exit(1);
     }
 
     fscanf(fin, "%d", n);
-    *v = (Att *) malloc((*n) * sizeof(Att));
+    *v = (Att *)malloc((*n) * sizeof(Att));
 
-    for (int i = 0; i < (*n); i++) {
+    for (int i = 0; i < (*n); i++)
+    {
         fscanf(fin, "%d %d", &((*v)[i].s), &((*v)[i].f));
     }
 
     fclose(fin);
 }
 
-void attSel(int n, Att *v) {
-    int *sol = (int *)calloc(n, sizeof(int)); // Soluzione corrente
-    int *bestSol = (int *)calloc(n, sizeof(int)); // Migliore soluzione
-    int bestDur = 0; // Massima durata trovata
+void attSel(int n, Att *v)
+{
+    int *sol = (int *)calloc(n, sizeof(int));
+    int *bestSol = (int *)calloc(n, sizeof(int));
+    int bestDur = 0;
 
     powerset(0, v, n, sol, bestSol, &bestDur, 0);
 
     printf("Migliore durata: %d\n", bestDur);
     printf("Attivita' selezionate:\n");
-    for (int i = 0; i < n; i++) {
-        if (bestSol[i] == 1) {
+    for (int i = 0; i < n; i++)
+    {
+        if (bestSol[i] == 1)
+        {
             printf("Attivita' %d: inizio=%d, fine=%d\n", i, v[i].s, v[i].f);
         }
     }
@@ -65,15 +73,17 @@ void attSel(int n, Att *v) {
     free(bestSol);
 }
 
-// Funzione per generare il powerset
-void powerset(int pos, Att *v, int n, int *sol, int *bestSol, int *bestDur, int currDur) {
-    if (pos >= n) {
-        // Verifica se la soluzione è compatibile
-        if (check(v, sol, n)) {
-            // Calcola la durata totale della soluzione corrente
-            if (currDur > *bestDur) {
+void powerset(int pos, Att *v, int n, int *sol, int *bestSol, int *bestDur, int currDur)
+{
+    if (pos >= n)
+    {
+        if (check(v, sol, n) == 1)
+        {
+            if (currDur > *(bestDur))
+            {
                 *bestDur = currDur;
-                for (int i = 0; i < n; i++) {
+                for (int i = 0; i < n; i++)
+                {
                     bestSol[i] = sol[i];
                 }
             }
@@ -81,23 +91,25 @@ void powerset(int pos, Att *v, int n, int *sol, int *bestSol, int *bestDur, int 
         return;
     }
 
-    // Escludi l'attività corrente
     sol[pos] = 0;
     powerset(pos + 1, v, n, sol, bestSol, bestDur, currDur);
-
-    // Includi l'attività corrente
     sol[pos] = 1;
     powerset(pos + 1, v, n, sol, bestSol, bestDur, currDur + (v[pos].f - v[pos].s));
 }
 
-// Funzione per verificare la compatibilità delle attività selezionate
-int check(Att *v, int *sol, int n) {
-    for (int i = 0; i < n; i++) {
-        if (sol[i]) {
-            for (int j = i + 1; j < n; j++) {
-                if (sol[j]) {
+int check(Att *v, int *sol, int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        if (sol[i] == 1)
+        {
+            for (int j = i + 1; j < n; j++)
+            {
+                if (sol[j] == 1)
+                {
                     // Verifica sovrapposizione
-                    if (!(v[i].f <= v[j].s || v[j].f <= v[i].s)) {
+                    if ((v[i].s < v[j].f) && (v[j].s < v[i].f))
+                    {
                         return 0;
                     }
                 }
--- Lab05/Ese02/main.c
+++ Lab05/Ese02/main.c
@@ -3,12 +3,14 @@
 #include <string.h>
 
 // TYPEDEF
-typedef struct {
+typedef struct
+{
     char color1, color2;
     int value1, value2;
 } Tile;
 
-typedef struct {
+typedef struct
+{
     int id;
     int rotation;
 } Cell;
@@ -16,15 +18,16 @@ typedef struct {
 // PROTOTIPI
 void read_file(char *filename, char *filename2, Tile **tiles, Cell **boards, int *t, int *r, int *c);
 
-int main() {
+int main()
+{
     // DICHIARAZIONE
     int r, c; // Righe R X C Colonne (matrice boards)
-    int t; // Tessere del gioco
+    int t;    // Tessere del gioco
 
     Tile *tiles;
     Cell *boards;
 
-    read_file("tiles.txt", "board.txt", &tiles, &boards, &t, &r, &c); //TODO: non funziona la lettura
+    read_file("tiles.txt", "board.txt", &tiles, &boards, &t, &r, &c); // TODO: non funziona la lettura
     // USO DISPOSIZIONI RIPETUTE, guardare le slide del sudoku
 
     free(tiles);
@@ -34,23 +37,27 @@ int main() {
 }
 
 // FUNCTIONS
-void read_file(char *filename, char *filename2, Tile **tiles, Cell **boards, int *t, int *r, int *c){
+void read_file(char *filename, char *filename2, Tile **tiles, Cell **boards, int *t, int *r, int *c)
+{
     FILE *fin;
 
     fin = fopen("tiles.txt", "r");
-    if(fin == NULL){
+    if (fin == NULL)
+    {
         printf("Errore nella lettura del file!");
         exit(1);
     }
 
     fscanf(fin, "%d", t);
-    *tiles = (Tile *) malloc((*t) * sizeof(Tile));
+    *tiles = (Tile *)malloc((*t) * sizeof(Tile));
 
-    for (int i = 0; i < (*t); i++) {
-        fscanf(fin, "%c %d %c %d", &((*tiles)[i].color1), &((*tiles)[i].value1), &((*tiles)[i].color2), &((*tiles)[i].value2));
+    for (int i = 0; i < *t; i++)
+    {
+        fscanf(fin, " %c %c %d %d", &(*tiles)[i].color1, &(*tiles)[i].color2, &(*tiles)[i].value1, &(*tiles)[i].value2);
     }
 
-    for (int i = 0; i < (*t); i++) {
+    for (int i = 0; i < (*t); i++)
+    {
         printf("%c %d %c %d", ((*tiles)[i].color1), ((*tiles)[i].value1), ((*tiles)[i].color2), ((*tiles)[i].value2));
         printf("\n");
     }
