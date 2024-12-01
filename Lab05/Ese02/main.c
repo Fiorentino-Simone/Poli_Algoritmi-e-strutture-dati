af2a2ef2f2215296d32605cad32d9c5b6ccf9e5e
Simone Fiorentino <simone.fiorentino@cambieri.it>
2024-12-01 14:38:14 +0100

    Close ese01, lab06

--- Lab05/Ese02/main.c
+++ Lab05/Ese02/main.c
@@ -5,8 +5,10 @@
 // TYPEDEF
 typedef struct
 {
-    char color1, color2;
-    int value1, value2;
+    char color1;
+    int value1;
+    char color2;
+    int value2;
 } Tile;
 
 typedef struct
@@ -18,6 +20,9 @@ typedef struct
 // PROTOTIPI
 void read_file(char *filename, char *filename2, Tile **tiles, Cell **boards, int *t, int *r, int *c);
 
+void stampa_tiles(Tile *tiles, int t);
+void stampa_boards(Cell *boards, int r, int c);
+
 int main()
 {
     // DICHIARAZIONE
@@ -27,7 +32,11 @@ int main()
     Tile *tiles;
     Cell *boards;
 
-    read_file("tiles.txt", "board.txt", &tiles, &boards, &t, &r, &c); // TODO: non funziona la lettura
+    read_file("tiles.txt", "board.txt", &tiles, &boards, &t, &r, &c); 
+
+    stampa_tiles(tiles, t);
+    stampa_boards(boards, r, c);
+
     // USO DISPOSIZIONI RIPETUTE, guardare le slide del sudoku
 
     free(tiles);
@@ -41,26 +50,53 @@ void read_file(char *filename, char *filename2, Tile **tiles, Cell **boards, int
 {
     FILE *fin;
 
-    fin = fopen("tiles.txt", "r");
+    fin = fopen(filename, "r");
     if (fin == NULL)
     {
         printf("Errore nella lettura del file!");
         exit(1);
     }
 
-    fscanf(fin, "%d", t);
+    fscanf(fin, "%d\n", t);
     *tiles = (Tile *)malloc((*t) * sizeof(Tile));
 
     for (int i = 0; i < *t; i++)
     {
-        fscanf(fin, " %c %c %d %d", &(*tiles)[i].color1, &(*tiles)[i].color2, &(*tiles)[i].value1, &(*tiles)[i].value2);
-    }
-
-    for (int i = 0; i < (*t); i++)
-    {
-        printf("%c %d %c %d", ((*tiles)[i].color1), ((*tiles)[i].value1), ((*tiles)[i].color2), ((*tiles)[i].value2));
-        printf("\n");
+        fscanf(fin, "%c %d %c %d\n", &(*tiles)[i].color1, &(*tiles)[i].value1, &(*tiles)[i].color2, &(*tiles)[i].value2);
     }
 
     fclose(fin);
+
+    fin = fopen(filename2, "r");
+    if (fin == NULL)
+    {
+        printf("Errore nella lettura del file!");
+        exit(1);
+    }
+
+    fscanf(fin, "%d %d", r, c);
+    *boards = (Cell *)malloc((*r) * (*c) * sizeof(Cell));
+
+    for (int i = 0; i < (*r) * (*c); i++) // TODO: gestire la rotazione
+    {
+        fscanf(fin, "%d %d\n", &(*boards)[i].id, &(*boards)[i].rotation);
+    }
+
+    fclose(fin);
+}
+
+void stampa_tiles(Tile *tiles, int t)
+{
+    for (int i = 0; i < t; i++)
+    {
+        printf("%c %d %c %d\n", tiles[i].color1, tiles[i].value1, tiles[i].color2, tiles[i].value2);
+    }
+}
+
+void stampa_boards(Cell *boards, int r, int c)
+{
+    for (int i = 0; i < r * c; i++)
+    {
+        printf("%d %d\n", boards[i].id, boards[i].rotation);
+    }
 }
--- Lab06/Ese01/main.c
+++ Lab06/Ese01/main.c
@@ -1,6 +1,118 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main() {
-    printf("Hello, World!\n");
+// TYPEDEF
+typedef struct
+{
+    int s, f;
+} Att;
+
+// PROTOTIPI
+void read_file(int *n, Att **v);
+
+void attSel(int n, Att *v);
+void displaySol(int *P, int i, Att *v);
+int compare(const void *a, const void *b);
+
+int main()
+{
+    // DICHIARAZIONI
+    int n;
+    Att *v;
+
+    read_file(&n, &v);
+
+    attSel(n, v);
+
+    free(v);
     return 0;
+}
+
+// FUNCTIONS
+void read_file(int *n, Att **v)
+{
+    FILE *fin;
+
+    fin = fopen("att.txt", "r");
+    if (fin == NULL)
+    {
+        printf("Impossibile aprire il file!");
+        exit(1);
+    }
+
+    fscanf(fin, "%d", n);
+    *v = (Att *)malloc((*n + 1) * sizeof(Att)); // +1 per la sentinella
+
+    for (int i = 1; i <= (*n); i++)
+    {
+        fscanf(fin, "%d %d", &((*v)[i].s), &((*v)[i].f));
+    }
+
+    // Sentinella
+    (*v)[0].s = 0;
+    (*v)[0].f = 0;
+
+    fclose(fin);
+}
+
+int compare(const void *a, const void *b)
+{
+    Att *att1 = (Att *)a;
+    Att *att2 = (Att *)b;
+    return att1->f - att2->f;
+}
+
+void attSel(int n, Att *v)
+{
+    int *dp = (int *)calloc(n + 1, sizeof(int));
+    int *P = (int *)calloc(n + 1, sizeof(int));
+    int maxDur = 0, last = 0;
+
+    // Ordina le attività per tempo di fine
+    qsort(v, n + 1, sizeof(Att), compare);
+
+    for (int i = 0; i <= n; i++)
+    {
+        dp[i] = 0;
+        P[i] = -1;
+    }
+
+    for (int i = 1; i <= n; i++)
+    {
+        dp[i] = v[i].f - v[i].s; // Durata corrente
+        P[i] = 0;
+
+        // Controllo del predecessore
+        for (int j = 0; j < i; j++)
+        {
+            if ((v[j].f <= v[i].s) && (dp[i] < dp[j] + (v[i].f - v[i].s)))
+            {
+                dp[i] = dp[j] + (v[i].f - v[i].s);
+                P[i] = j;
+            }
+        }
+
+        if (dp[i] > maxDur)
+        {
+            maxDur = dp[i];
+            last = i;
+        }
+    }
+
+    printf("Migliore durata: %d\n", maxDur);
+    printf("Attivita' selezionate:\n");
+    displaySol(P, last, v);
+
+    free(dp);
+    free(P);
+}
+
+void displaySol(int *P, int i, Att *v)
+{
+    if (i == 0)
+        return;
+
+    displaySol(P, P[i], v);
+
+    printf("Attivita': inizio=%d, fine=%d\n", v[i].s, v[i].f);
 }
