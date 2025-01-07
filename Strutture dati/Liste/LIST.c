253eebe1b0e7f2ec993bc1364096e094835310b8
Simone Fiorentino <simone.fiorentino@cambieri.it>
2025-01-07 09:53:32 +0100

    Complete List Data structure

--- Strutture dati/Liste/LIST.c
+++ Strutture dati/Liste/LIST.c
@@ -1,17 +1,179 @@
 #include "list.h"
 
-// DICHIARAZIONI: per LIST ADT di prima classe
-struct list { link head; link tail; int N; };
+// DEFINIZIONI: node (quasi ADT) e list (ADT di prima classe)
+typedef struct node *link;
+struct node { Item item; link next; };
+struct list { link head; int N; };
 
 // FUNCTIONS
-link NEWnode(Item val, link next){
+static link NEWnode(Item val, link next){
     link x = malloc(sizeof *x);
     x->item = val;
     x->next = next;
     return x;
 }
 
+LIST LISTinit(){
+    LIST l = malloc(sizeof *l);
+    l->head = NULL;
+    l->N = 0;
+    return l;
+}
+
+void LISTfree(link h){
+    if (h == NULL) return;
+    LISTfree(h->next);
+    free(h);
+
+    // senno con ciclo while (non ricorsivo)
+    // link x = h;
+    // while (x != NULL){
+    //     link t = x;
+    //     x = x->next;
+    //     free(t);
+    // }
+}
+
 void LISTinsHead(LIST l, Item val){
     l->head = NEWnode(val, l->head);
     l->N++;
+}
+
+link LISTinsTail(link head, Item val){
+    if(head == NULL) return NEWnode(val, NULL);
+    head->next = LISTinsTail(head->next, val);
+    return head;
+}
+
+Item LISTsearch(link head, Key k){
+    for (link x = head; x != NULL; x = x->next){
+        if (KEYcompare(KEYget(x->item), k) == 0){
+            return x->item;
+        }
+    }
+    return NULLitem();
+}
+
+link LISTdelhead(link head){
+    link x = head->next;
+    free(head);
+    return x;
+}
+
+Item LISTextrheadP(link *hp){
+    // funzione che estrae l'elemento in testa alla lista
+    // e lo restituisce come Item
+    if (*hp == NULL) return NULLitem();
+    link x = *hp;
+    Item val = x->item;
+    *hp = x->next;
+    free(x);
+    return val;
+}
+
+link LISTdelkey(link h, Key k){
+    // funzione che cancella il primo nodo con chiave k
+    // e restituisce il puntatore al nodo successivo
+    link x, p;
+    for (x = h, p = NULL; x != NULL; p = x, x = x->next){
+        if (KEYcompare(KEYget(x->item), k) == 0){
+            if (p == NULL){
+                h = x->next;
+            } else {
+                p->next = x->next;
+            }
+            free(x);
+            return h;
+        }
+    }
+    return h;
+}
+
+link LISTdelkeyR(link x, Key k){
+    // variante ricorsiva di LISTdelkey
+    if (x == NULL) return NULL;
+    if (KEYcompare(KEYget(x->item), k) == 0){
+        link t = x->next;
+        free(x);
+        return t;
+    }
+    x->next = LISTdelkeyR(x->next, k);
+    return x;
+}
+
+Item LISTextrkeyP(link *x, Key k){
+    // funzione che estrae l'elemento con chiave k
+    // e lo restituisce come Item in maniera iterativa
+    link p, t;
+    for (p = NULL, t = *x; t != NULL; p = t, t = t->next){
+        if (KEYcompare(KEYget(t->item), k) == 0){
+            if (p == NULL){
+                *x = t->next;
+            } else {
+                p->next = t->next;
+            }
+            Item val = t->item;
+            free(t);
+            return val;
+        }
+    }
+}
+
+link LISTsortins(link h, Item item){
+    // funzione che inserisce un nodo in una lista ordinata (per chiave)
+    link x, p;
+    for (x = h, p = NULL; x != NULL; p = x, x = x->next){
+        if (KEYcompare(KEYget(x->item), KEYget(item)) > 0){
+            break;
+        }
+    }
+    link new = NEWnode(item, x);
+    if (p == NULL){
+        return new;
+    } else {
+        p->next = new;
+        return h;
+    }
+}
+
+Item LISTsortsearch(link h, Key k){
+    // TODO: da verificare
+    // funzione che cerca un nodo in una lista ordinata (per chiave)
+    for (link x = h; x != NULL; x = x->next){
+        if (KEYcompare(KEYget(x->item), k) == 0){
+            return x->item;
+        }
+        if (KEYcompare(KEYget(x->item), k) > 0){
+            break;
+        }
+    }
+    return NULLitem();
+}
+
+link LISTsortdel(link h, Key k){
+    // funzione che cancella il primo nodo con chiave k
+    // e restituisce il puntatore aggiornato alla lista
+    link x, p;
+    for (x = h, p = NULL; x != NULL; p = x, x = x->next){
+        if (KEYcompare(KEYget(x->item), k) == 0){
+            if (p == NULL){
+                h = x->next;
+            } else {
+                p->next = x->next;
+            }
+            free(x);
+            return h;
+        }
+        if (KEYcompare(KEYget(x->item), k) > 0){
+            break;
+        }
+    }
+    return h;
+}
+
+void LISTshow(link h){
+    // funzione che stampa tutti gli elementi della lista
+    for (link x = h; x != NULL; x = x->next){
+        ITEMshow(x->item);
+    }
 }
--- Strutture dati/Liste/LIST.h
+++ Strutture dati/Liste/LIST.h
@@ -4,26 +4,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// sarebbe da importare la libreria KEY.h e ITEM.h che sarebbero dei quasi ADT
 typedef int Key;
 typedef int Item;
 
-typedef struct node *link;
-struct node { Item item; link next; };
-typedef struct list *LIST;
+typedef struct list *LIST; // ADT lista espresso come ADT di prima classe
 
-link NEWnode(Item val, link next);
+// PROTOTIPI
+// creatore e distruttore
+LIST LISTinit();
+void LISTfree(link h);
+
+// inserimento
 void LISTinsHead(LIST l, Item val);
 link LISTinsTail(link head, Item val);
+
 Item LISTsearch(link head, Key k);
-link LISTdelhead(link h);
+
+// cancellazioni
+link LISTdelhead(link head);
 Item LISTextrheadP(link *hp);
 link LISTdelkey(link h, Key k);
 link LISTdelkeyR(link x, Key k);
 Item LISTextrkeyP(link *x, Key k);
+
+// ordinamenti
 link LISTsortins(link h, Item item);
 Item LISTsortsearch(link h, Key k);
 link LISTsortdel(link h, Key k);
+
 void LISTshow(link h);
-void LISTfree(link h);
 
 #endif //LIST_H
